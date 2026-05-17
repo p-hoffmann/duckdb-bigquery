bc81ece885f60f8b5f712434abc3245d42996569
hafenkran <4216598+hafenkran@users.noreply.github.com>
2026-05-17 23:48:38 +0200

    Fix Windows DML enum names
    (cherry picked from commit 93fc31987059d4707fdc677c320bc8c784a4dd52)

--- src/bigquery_client.cpp
+++ src/bigquery_client.cpp
@@ -188,9 +188,9 @@ static google::cloud::bigquery::v2::QueryParameter BuildPositionalQueryParameter
 
 static const char *DmlStatementName(BigqueryDmlStatementType statement_type) {
     switch (statement_type) {
-    case BigqueryDmlStatementType::UPDATE:
+    case BigqueryDmlStatementType::DML_UPDATE:
         return "UPDATE";
-    case BigqueryDmlStatementType::DELETE:
+    case BigqueryDmlStatementType::DML_DELETE:
         return "DELETE";
     }
     throw InternalException("Unknown BigQuery DML statement type");
@@ -199,12 +199,12 @@ static const char *DmlStatementName(BigqueryDmlStatementType statement_type) {
 static std::optional<int64_t> ExtractDmlStatsAffectedRows(const google::cloud::bigquery::v2::DmlStats &dml_stats,
                                                           BigqueryDmlStatementType statement_type) {
     switch (statement_type) {
-    case BigqueryDmlStatementType::UPDATE:
+    case BigqueryDmlStatementType::DML_UPDATE:
         if (dml_stats.has_updated_row_count()) {
             return dml_stats.updated_row_count().value();
         }
         return std::nullopt;
-    case BigqueryDmlStatementType::DELETE:
+    case BigqueryDmlStatementType::DML_DELETE:
         if (dml_stats.has_deleted_row_count()) {
             return dml_stats.deleted_row_count().value();
         }
